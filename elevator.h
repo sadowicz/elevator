ea5d536b9e30a347649d0253781d1355c508fb6d
Filip Sądowicz <sadowicz31@gmail.com>
2021-04-17 13:36:09 +0200

    Adding elevator data structures

--- elevator.h
+++ elevator.h
@@ -1,4 +1,15 @@
 #ifndef ELEVATOR_H
 #define ELEVATOR_H
 
+typedef struct ElevatorInfo {
+    uint8_t id;
+    uint8_t currentFloor;
+    uint8_t destination;
+}ElevatorInfo;
+
+typedef struct ElevatorCollection {
+    ElevatorInfo** data;
+    uint8_t amount;
+};
+
 #endif
